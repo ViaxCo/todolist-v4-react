71c6151db5da9ebe30b1ef64e508ce5a89ea5f4c
Victor Ajibade <ajibadevic@gmail.com>
2021-01-28 18:14:37 +0100

    Add spring animation on delete

--- client/src/components/ListCard.tsx
+++ client/src/components/ListCard.tsx
@@ -35,67 +35,76 @@ const ListCard = ({ list, i }: Props) => {
   const { deleteList, setIsLoading } = useContext(GlobalContext);
 
   return (
-    <MotionFlex
-      align="center"
-      minH="70px"
-      borderBottom="1px solid"
-      borderColor={useColorModeValue("#f1f1f1", "viaxco.400")}
-      // Mount and exit animations of each card
-      opacity="0"
-      variants={{
-        hidden: (i: number) => ({ opacity: 0, y: -50 * i }),
-        visible: (i: number) => ({
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: i * 0.025,
-          },
-        }),
-        exit: {
-          opacity: 0,
-          x: -50,
-        },
+    <motion.div
+      layout
+      transition={{
+        type: "spring",
+        stiffness: 600,
+        damping: 30,
       }}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      custom={i}
     >
-      <Box
-        w={{ base: "82%", md: "83%" }}
-        cursor="pointer"
-        display="flex"
-        alignItems="center"
+      <MotionFlex
+        align="center"
+        minH="70px"
+        borderBottom="1px solid"
+        borderColor={useColorModeValue("#f1f1f1", "viaxco.400")}
+        // Mount and exit animations of each card
+        opacity="0"
+        variants={{
+          hidden: (i: number) => ({ opacity: 0, y: -50 * i }),
+          visible: (i: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: i * 0.025,
+            },
+          }),
+          exit: {
+            opacity: 0,
+            x: -50,
+          },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        custom={i}
       >
-        {/* Combine react-router-dom Link and @chakra-ui Link props together */}
-        <Link
-          as={RouterLink}
-          to={{ pathname: `/api/${list.name}` }}
-          flex="1"
-          textAlign="center"
-          borderRadius="5px"
-          onClick={() => setIsLoading && setIsLoading(true)}
+        <Box
+          w={{ base: "82%", md: "83%" }}
+          cursor="pointer"
+          display="flex"
+          alignItems="center"
         >
-          <Text
-            p="20px"
-            fontSize="1.2rem"
-            fontWeight={400}
-            color={useColorModeValue("main.blue", "white")}
+          {/* Combine react-router-dom Link and @chakra-ui Link props together */}
+          <Link
+            as={RouterLink}
+            to={{ pathname: `/api/${list.name}` }}
+            flex="1"
+            textAlign="center"
+            borderRadius="5px"
+            onClick={() => setIsLoading && setIsLoading(true)}
           >
-            {list.name}
-          </Text>
-        </Link>
-      </Box>
-      <IconButton
-        m="auto"
-        aria-label="Delete List"
-        variant="ghost"
-        size={useBreakpointValue({ base: "sm", md: "md" })}
-        colorScheme="red"
-        onClick={() => list._id && deleteList && deleteList(list._id)}
-        icon={<DeleteIcon />}
-      />
-    </MotionFlex>
+            <Text
+              p="20px"
+              fontSize="1.2rem"
+              fontWeight={400}
+              color={useColorModeValue("main.blue", "white")}
+            >
+              {list.name}
+            </Text>
+          </Link>
+        </Box>
+        <IconButton
+          m="auto"
+          aria-label="Delete List"
+          variant="ghost"
+          size={useBreakpointValue({ base: "sm", md: "md" })}
+          colorScheme="red"
+          onClick={() => list._id && deleteList && deleteList(list._id)}
+          icon={<DeleteIcon />}
+        />
+      </MotionFlex>
+    </motion.div>
   );
 };
 
--- client/src/components/ListItemCard.tsx
+++ client/src/components/ListItemCard.tsx
@@ -36,74 +36,83 @@ const ListItemCard = ({ item, i, customListName }: Props) => {
   const { deleteListItem, toggleItemCompleted } = useContext(GlobalContext);
 
   return (
-    <MotionFlex
-      align="center"
-      minH="70px"
-      borderBottom="1px solid"
-      borderColor={useColorModeValue("#f1f1f1", "viaxco.400")}
-      // Mount and exit animations of each card
-      opacity="0"
-      variants={{
-        hidden: (i: number) => ({ opacity: 0, y: -50 * i }),
-        visible: (i: number) => ({
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: i * 0.025,
-          },
-        }),
-        exit: {
-          opacity: 0,
-          x: -50,
-        },
+    <motion.div
+      layout
+      transition={{
+        type: "spring",
+        stiffness: 600,
+        damping: 30,
       }}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      custom={i}
     >
-      <Box
-        w={{ base: "82%", md: "83%" }}
-        cursor="pointer"
-        display="flex"
-        alignItems="center"
+      <MotionFlex
+        align="center"
+        minH="70px"
+        borderBottom="1px solid"
+        borderColor={useColorModeValue("#f1f1f1", "viaxco.400")}
+        // Mount and exit animations of each card
+        opacity="0"
+        variants={{
+          hidden: (i: number) => ({ opacity: 0, y: -50 * i }),
+          visible: (i: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: i * 0.025,
+            },
+          }),
+          exit: {
+            opacity: 0,
+            x: -50,
+          },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        custom={i}
       >
-        <Checkbox
-          flex="1"
-          colorScheme="viaxco"
-          p="20px"
-          size="lg"
-          spacing="2rem"
-          fontWeight={400}
-          color={useColorModeValue("main.blue", "white")}
-          isChecked={checked}
-          onChange={e => {
-            setChecked(!checked);
-            item._id &&
-              customListName &&
-              toggleItemCompleted &&
-              toggleItemCompleted(customListName, item._id, !checked);
-          }}
-          textDecoration={checked ? "line-through" : undefined}
+        <Box
+          w={{ base: "82%", md: "83%" }}
+          cursor="pointer"
+          display="flex"
+          alignItems="center"
         >
-          {item.task}
-        </Checkbox>
-      </Box>
-      <IconButton
-        m="auto"
-        aria-label="Delete Item"
-        variant="ghost"
-        size={useBreakpointValue({ base: "sm", md: "md" })}
-        colorScheme="red"
-        onClick={() =>
-          customListName &&
-          item._id &&
-          deleteListItem &&
-          deleteListItem(customListName, item._id)
-        }
-        icon={<DeleteIcon />}
-      />
-    </MotionFlex>
+          <Checkbox
+            flex="1"
+            colorScheme="viaxco"
+            p="20px"
+            size="lg"
+            spacing="2rem"
+            fontWeight={400}
+            color={useColorModeValue("main.blue", "white")}
+            isChecked={checked}
+            onChange={e => {
+              setChecked(!checked);
+              item._id &&
+                customListName &&
+                toggleItemCompleted &&
+                toggleItemCompleted(customListName, item._id, !checked);
+            }}
+            textDecoration={checked ? "line-through" : undefined}
+          >
+            {item.task}
+          </Checkbox>
+        </Box>
+        <IconButton
+          m="auto"
+          aria-label="Delete Item"
+          variant="ghost"
+          size={useBreakpointValue({ base: "sm", md: "md" })}
+          colorScheme="red"
+          onClick={() =>
+            customListName &&
+            item._id &&
+            deleteListItem &&
+            deleteListItem(customListName, item._id)
+          }
+          icon={<DeleteIcon />}
+        />
+      </MotionFlex>
+    </motion.div>
   );
 };
 
